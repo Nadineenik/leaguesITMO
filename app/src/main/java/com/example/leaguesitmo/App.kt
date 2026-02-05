3ca6623dae0b4360ff1d91f8f32fd3c11ce8837f
NADEZHDA <128965468+Nadineenik@users.noreply.github.com>
2026-02-05 17:15:52 +0300

    autorization

--- app/src/main/java/com/example/leaguesitmo/App.kt
+++ app/src/main/java/com/example/leaguesitmo/App.kt
@@ -1,45 +1,37 @@
 package com.example.leaguesitmo
 
+
 import android.app.Application
 import androidx.room.Room
-import androidx.room.migration.Migration
-import androidx.sqlite.db.SupportSQLiteDatabase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import com.example.leaguesitmo.data.AppDatabase
 
-
 class App : Application() {
     companion object {
         val database: AppDatabase by lazy {
+            // Это выполнится при первом обращении
             Room.databaseBuilder(
                 instance.applicationContext,
                 AppDatabase::class.java,
-                "league_app.db"
+                "student_app.db"
             )
-                .addMigrations(MIGRATION_1_2)
                 .fallbackToDestructiveMigration()
-                .allowMainThreadQueries()
                 .build()
         }
 
-        // Самые надёжные миграции
-        val MIGRATION_1_2 = object : Migration(1, 2) {
-            override fun migrate(db: SupportSQLiteDatabase) {
-                db.execSQL("ALTER TABLE users ADD COLUMN email TEXT")
-            }
-        }
-
-            lateinit var instance: App
+        lateinit var instance: App
             private set
     }
 
     override fun onCreate() {
         super.onCreate()
         instance = this
+
+        // Предзагрузка (опционально)
         GlobalScope.launch(Dispatchers.IO) {
-            database // просто инициализируем
+            database // просто вызов, чтобы инициализировать
         }
     }
 }
